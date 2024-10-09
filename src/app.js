2a93e160f3d2ccd69cd0d85e0c89e5676347210a
Yash <shuklayash1304@gmail.com>
2024-10-09 12:41:14 +0530

    Error Handling using Error Middleware

--- src/app.js
+++ src/app.js
@@ -1,25 +1,23 @@
 const express = require("express");
-const { adminAuth, userAuth } = require("./middlewares/auth");
+// const { adminAuth, userAuth } = require("./middlewares/auth");
 
 // Creating an express instance / application of express / creating a server
 const app = express();
 
-app.use("/admin", adminAuth);
-
-app.post("/user/login", (req, res) => {
-  res.send("User Logged In Successfully");
+app.get("/getUserData", (req, res) => {
+  // try {
+    throw new Error("ahdshdb");
+    res.send("User Data Sent");
+  // } catch (err) {
+    res.status(500).send("Somehting Went Wrong 2");
+  // }
 });
 
-app.get("/user/data", userAuth, (req, res) => {
-  res.send("User Data Sent");
-});
-
-app.get("/admin/getAllData", (req, res) => {
-  res.send("All Data Sent");
-});
-
-app.get("/admin/deleteUser", (req, res) => {
-  res.send("Deltet a user");
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    // Log Error
+    res.status(500).send("Something Went Wrong");
+  }
 });
 
 // Listening on some port for getting incoming requests
