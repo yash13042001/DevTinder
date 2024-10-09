0a4c6010e831c1247eee4fda1abfe3aa4a814aa3
Yash <shuklayash1304@gmail.com>
2024-10-09 15:47:10 +0530

    Creating User Schema and User Model

--- src/config/database.js
+++ src/config/database.js
@@ -2,10 +2,8 @@ const mongoose = require("mongoose");
 
 const connectDb = async () => {
   await mongoose.connect(
-    "mongodb+srv://shuklayash1304:Yash1304@yashcluster.9e4xf.mongodb.net/?retryWrites=true&w=majority&appName=yashcluster"
+    "mongodb+srv://shuklayash1304:Yash1304@yashcluster.9e4xf.mongodb.net/?retryWrites=true&w=majority&appName=yashcluster/devTinder"
   );
 };
 
 module.exports = connectDb;
-
-
